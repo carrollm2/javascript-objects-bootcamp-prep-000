cd6997da3c73c93d62c04f9434cc3e3d5be34da9
Michael Carroll <carrollm2@gmail.com>
2017-11-15 12:37:53 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  TomPetty: "Learning to Fly"
+  Beatles: "Here Comes the Sun"
+}
+
+function updatePlaylist(obj, key, value){
+  return Object.assign({}, obj, {[key]: value})
+}
+
+/*
+function removeFromPlaylist(obj, key, value){
+  delete obj[key]
+  return Object.assign({}, obj, {[key]: value})
+  
+}*/
