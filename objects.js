9b7f6288f7148ea5763a5e298378185aa4bbbc64
Michael Carroll <carrollm2@gmail.com>
2017-11-15 12:43:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,9 +4,8 @@ function updatePlaylist(obj, key, value){
   return Object.assign({}, obj, {[key]: value})
 }
 
-/*
+
 function removeFromPlaylist(obj, key, value){
   delete obj[key]
   return Object.assign({}, obj, {[key]: value})
-
-}*/
+}
